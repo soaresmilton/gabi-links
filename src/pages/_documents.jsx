f3c25ad9ee9aeec72f3bfdb880977545a4897e07
soaresmilton <eng.milton.soares@gmail.com>
2021-06-21 21:08:54 -0300

    FavIcon Fix

--- src/pages/_documents.jsx
+++ src/pages/_documents.jsx
@@ -10,9 +10,10 @@ class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-          integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-          crossorigin="anonymous" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+            integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+            crossorigin="anonymous" />
+          <link rel="icon" href="/images/GartIcon.svg" />
         </Head>
         <body>
           <Main />
--- src/pages/links/gartlemos-links.jsx
+++ src/pages/links/gartlemos-links.jsx
@@ -11,6 +11,8 @@ export default function GartlemosLinks() {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
           integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
           crossorigin="anonymous" />
+        <link rel="icon" href="/images/GartIcon.svg" />
+
       </Head>
 
       <main className={styles.container}>
